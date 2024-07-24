5652ce5d37a0686893e34db1713b8756e09f0974
hrjugar <hernanjugar123@gmail.com>
2024-07-24 17:44:37 +0800

    Added placeholder UI for doctor navbar

--- frontend/src/layouts/doctor/DoctorNavbar.tsx
+++ frontend/src/layouts/doctor/DoctorNavbar.tsx
@@ -1,7 +1,53 @@
+import { NavLink } from "react-router-dom";
+
+interface DoctorNavLink {
+  to: string;
+  // icon: React.ReactNode; 
+}
+
+const primaryNavLinks: DoctorNavLink[] = [
+  {
+    to: "/doctor"
+  },
+  {
+    to: "/doctor/appointments"
+  },
+  {
+    to: "/doctor/patients"
+  },
+  {
+    to: "/doctor/account"
+  },
+];
+
 export default function DoctorNavbar() {
   return (
-    <nav className="flex flex-col justify-start items-center bg-red-400">
-      <p>Doc Nav Test</p>
+    <nav className="flex flex-row justify-center items-stretch p-2">
+      <div className="flex flex-col justify-between items-center rounded-md shadow-md p-4 bg-white gap-6">
+        <div className="flex flex-col justify-start items-center gap-6">
+          {primaryNavLinks.map((link) => (
+            <NavLink
+              to={link.to}
+              className={({ isActive }) => `
+                size-12 flex justify-center items-center rounded-md 
+                ${isActive ? 'bg-accent' : ''}
+              `}
+              key={`link-${link.to}`}
+            >
+              <div className="size-8 bg-gray-400 rounded-full" /> {/* icon */}
+            </NavLink>
+          ))}
+        </div>
+
+        <div className="flex flex-col justify-end items-center gap-6">
+          <button 
+            className="size-12 flex justify-center items-center"
+            onClick={() => console.log("logout")} // TODO: Implement this
+          >
+            <div className="size-8 bg-gray-400 rounded-full"/> {/* icon */}
+          </button>
+        </div>
+      </div>
     </nav>
   )
 }
--- frontend/src/layouts/doctor/index.tsx
+++ frontend/src/layouts/doctor/index.tsx
@@ -3,7 +3,7 @@ import DoctorNavbar from "./DoctorNavbar";
 
 export default function DoctorLayout() {
   return (
-    <div className="w-full min-h-screen flex-grow flex flex-row justify-start items-stretch">
+    <div className="w-full min-h-screen flex-grow flex flex-row justify-start items-stretch bg-gray-50">
       <DoctorNavbar />
       <Outlet />
     </div>
