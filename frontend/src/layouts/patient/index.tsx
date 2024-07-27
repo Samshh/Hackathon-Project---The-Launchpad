b056d27e564dd544e5e449aca5ed3d691e9fe0b5
Samshh <samdacs2@gmail.com>
2024-07-27 17:48:59 +0800

    checkpoints for auth added please fix backend

--- frontend/src/layouts/patient/index.tsx
+++ frontend/src/layouts/patient/index.tsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import PatientHeader from "./PatientHeader";
 import GlobalSheet from "@/components/GlobalSheet";
+import axios from "axios";
 
 export default function PatientLayout() {
+  
+  const auth = axios.get("http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:4943/authenticate", {withCredentials: true});
+  
+  if (!auth) {
+    return <Navigate to="/login"/>;
+  }
+
   return (
     <div className="flex flex-col min-h-screen gap-4 px-8 py-6 bg-gray-50">
       <GlobalSheet/>
