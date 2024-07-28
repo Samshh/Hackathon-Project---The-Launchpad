01e14120487a7ae1a3496eac9bbb4bb771d94994
Jekko07 <jerichojamesranara@gmail.com>
2024-07-28 21:12:34 +0800

    test

--- frontend/src/pages/patient/index.tsx
+++ frontend/src/pages/patient/index.tsx
@@ -2,7 +2,7 @@ import CurrentAppointmentSection from '../../components/patient/dashboard/Curren
 import PrescriptionsSection from '../../components/patient/dashboard/PrescriptionsSection';
 import YourDoctorsSection from '../../components/patient/dashboard/yourDoctorsSection/YourDoctorsSection';
 import PastAppointmentsSection from '@/components/patient/dashboard/pastAppointmentsSection/PastAppointmentsSection';
-
+//
 export default function PatientDashboardPage() {
   return (
     <main className="grid flex-1 grid-cols-9 overflow-y-hidden gap-x-4 gap-y-4">
